266147ca6bc10b60fc48594eba66ff2bd269ff04
ctokheim <collintokheim@gmail.com>
2017-02-18 12:57:44 -0500

    Updated driver list overlapping code

--- protocol/cgc_overlap.py
+++ protocol/cgc_overlap.py
@@ -7,11 +7,12 @@ import utils
 import logging
 logger = logging.getLogger(__name__)
 
-def num_cgc_overlap(signif_genes, cgc_list):
+def num_cgc_overlap(signif_genes, cancer_type, cgc_list):
     """Intersect significant genes with CGC or other driver gene list."""
     cgc_result = {}
     for method in signif_genes:
-        intersect = len(set(signif_genes[method]) & set(cgc_list))
+        mygenes = signif_genes[method].get(cancer_type, [])
+        intersect = len(set(mygenes) & set(cgc_list))
         cgc_result[method] = intersect
     return cgc_result
 
@@ -29,36 +30,54 @@ def main(opts):
 
     # get the significant genes for each method
     signif_dict = utils.fetch_significant_genes(opts['input_dir'],
-                                                opts['qvalue'],
                                                 config)
     num_methods = len(signif_dict)
 
     # read in driver genes
-    if opts['cgc']:
-        # get cgc genes
-        driver_genes = utils.process_cgc(opts['cgc'])
-        list_name = 'CGC'
-    else:
-        # read in custom driver gene list
-        driver_genes = read_custom_list(opts['gene_list'])
-        list_name = 'Custom List'
+    driver_list_names = ['cgc', 'landscapes', 'kandoth',
+                         'high_confidence_list', 'gene_list']
+    driver_genes = set()
+    for l in driver_list_names:
+        # skip if list not provided
+        if opts[l] is None: continue
 
-    # count the overlap
-    num_cgc_dict = num_cgc_overlap(signif_dict, driver_genes)
-    num_signif_dict = {k: len(signif_dict[k]) for k in signif_dict}
+        # add genes to list
+        if l == 'cgc':
+            # get cgc genes
+            driver_genes |= set(utils.process_cgc(opts['cgc']))
+            list_name = 'CGC'
+        else:
+            # read in custom driver gene list
+            driver_genes |= set(read_custom_list(opts[l]))
+            list_name = 'Custom List'
+    driver_genes = list(driver_genes)
 
-    # format result
-    overlap_df = pd.DataFrame({'# '+list_name: pd.Series(num_cgc_dict),
-                               '# significant': pd.Series(num_signif_dict)})
-    overlap_df['Fraction overlap ({0})'.format(list_name)] = overlap_df['# '+list_name].astype(float) / overlap_df['# significant']
+    all_overlap_list = []
+    cancer_types = signif_dict[signif_dict.keys()[0]].keys()
+    for cancer_type in cancer_types:
+        # count the overlap
+        num_cgc_dict = num_cgc_overlap(signif_dict, cancer_type, driver_genes)
+        num_signif_dict = {k: len(signif_dict[k].get(cancer_type, []))
+                           for k in signif_dict}
+
+        # format result
+        overlap_df = pd.DataFrame({'# '+list_name: pd.Series(num_cgc_dict),
+                                '# significant': pd.Series(num_signif_dict)})
+        frac_ovlp_col = 'Fraction overlap ({0})'.format(list_name)
+        overlap_df[frac_ovlp_col] = overlap_df['# '+list_name].astype(float) / overlap_df['# significant']
+        overlap_df[frac_ovlp_col] = overlap_df[frac_ovlp_col].fillna(0)
+
+        # append result
+        overlap_df['CODE'] = cancer_type
+        all_overlap_list.append(overlap_df)
+
+        if opts['plot']:
+            import plot_data
+            cgc_path = os.path.join(opts['output'], cancer_type+'.driver_list_overlap.pdf')
+            plot_data.cgc_overlap(overlap_df, cgc_path, list_name)
 
     # save result
     cgc_path = os.path.join(opts['output'], 'gene_list_overlap.txt')
-    overlap_df.to_csv(cgc_path, sep='\t')
-
-    if opts['plot']:
-        import plot_data
-        cgc_path = os.path.join(opts['output'], 'gene_list_overlap.pdf')
-        plot_data.cgc_overlap(overlap_df, cgc_path, list_name)
+    pd.concat(all_overlap_list).to_csv(cgc_path, sep='\t')
 
     logger.info('Finished list_overlap sub-command.')
