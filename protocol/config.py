ed14e570e6ed6b1f7fe41549f94ccb5645b88152
ctokheim <collintokheim@gmail.com>
2017-02-25 19:25:18 -0500

    Changed config function name, and added taking top ranked genes

--- protocol/config.py
+++ protocol/config.py
@@ -1,10 +1,22 @@
 
-def fetch_gene_level_names(config):
+
+def fetch_level_names(config, level='gene', exclude=False):
     """Get all method names that predict genes."""
+    # get methods that will be excluded
+    if exclude:
+        exclude_list = config.get('exclude', [])
+    else:
+        exclude_list = []
+
     gene_methods = []
     for meth_name in config:
+        # skip if excluded
+        if meth_name in exclude_list: continue
+        # skip if not a method
+        if meth_name == 'exclude': continue
+
         lvl = config[meth_name].get('level', None)
-        if lvl.lower() == 'gene':
+        if str(lvl).lower() == level:
             gene_methods.append(meth_name)
     return gene_methods
 
@@ -33,6 +45,10 @@ def fetch_threshold(config, meth_name):
             thresh_col = 'score'
         elif config[meth_name]['threshold'].has_key('qvalue'):
             thresh_col = 'qvalue'
+        elif config[meth_name]['threshold'].has_key('pvalue'):
+            thresh_col = 'pvalue'
+        elif config[meth_name]['threshold'].has_key('rank'):
+            thresh_col = 'rank'
         thresh_val = float(config[meth_name]['threshold'][thresh_col])
         direction = config[meth_name]['threshold']['top']
         return thresh_col, thresh_val, direction
