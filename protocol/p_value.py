7fcd4eb5f013bc3b59acdca16c965a72b111f9f9
ctokheim <collintokheim@gmail.com>
2017-02-25 19:27:55 -0500

    Changed code to output a MLFC matrix and changed gene blacklist to be based on several lists

--- protocol/p_value.py
+++ protocol/p_value.py
@@ -4,16 +4,13 @@ import eval_stats
 import utils
 import config as cfg
 import os
+import cgc_overlap
 
 # logging
 import logging
 logger = logging.getLogger(__name__)
 
 def calculate_mlfc(df, meth_name, config):
-    # check whether there is custom p-value columns
-    #if cfg.is_valid_config(config, meth_name, 'pvalue'):
-        #pval_cols = config[meth_name]['pvalue']
-    #else:
     pval_cols = ['pvalue']
 
     # calculate the mlfc scores for each pvalue column
@@ -34,25 +31,40 @@ def main(opts):
     # load config
     config = utils.load_config(opts['config'])
 
-    # get full data for each method
-    full_data = utils.fetch_filtered_dataframes(opts['input_dir'],
-                                                opts['output'],
-                                                opts['min'],
-                                                opts['cgc'])
+    # Read in driver lists
+    cgc = utils.process_cgc(opts['cgc'])
+    landscapes = cgc_overlap.read_custom_list(opts['landscapes'])
+    kandoth = cgc_overlap.read_custom_list(opts['kandoth'])
+    tamborero = cgc_overlap.read_custom_list(opts['high_confidence_list'])
+    blacklist = set(cgc) | set(landscapes) | set(kandoth) | set(tamborero)
 
-    # compute MLFC scores
-    mlfc_result = {m: calculate_mlfc(full_data[m], m, config)
-                   for m in full_data}
-    mlfc_series = pd.Series(mlfc_result)
+    # iterate over methods
+    gene_methods = cfg.fetch_level_names(config, level='gene')
+    mlfc_list = []
+    for method_name in gene_methods:
+        # get full data for each method
+        full_data = utils.fetch_filtered_dataframes(opts['input_dir'],
+                                                    blacklist,
+                                                    method_name)
+
+        # compute MLFC scores
+        mlfc_result = {m: calculate_mlfc(full_data[m], m, config)
+                       for m in full_data}
+        mlfc_series = pd.Series(mlfc_result)
+        mlfc_series.name = method_name
+        mlfc_list.append(mlfc_series)
+
+    # creat a dataframe
+    mlfc_df = pd.concat(mlfc_list, axis=1)
 
     # save output
     mlfc_path = os.path.join(opts['output'], 'mlfc_scores.txt')
-    mlfc_series.to_csv(mlfc_path, sep='\t')
+    mlfc_df.to_csv(mlfc_path, sep='\t')
 
-    if opts['plot']:
-        import plot_data
-        mlfc_path = os.path.join(opts['output'], 'mlfc_scores.pdf')
-        plot_data.mlfc_score(mlfc_series, mlfc_path)
+    #if opts['plot']:
+        #import plot_data
+        #mlfc_path = os.path.join(opts['output'], 'mlfc_scores.pdf')
+        #plot_data.mlfc_score(mlfc_series, mlfc_path)
 
     logger.info('Finished p-value sub-command.')
-    return mlfc_result
+    return mlfc_df
